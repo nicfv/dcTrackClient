3aa02868173aadc2eb4d4e563c831cdccc7d6235
ventura <ventura@lbl.gov>
2023-04-10 17:36:19 -0700

    Finished adding api endpoints for makes, models, and connectors

--- src/dcTrackClient/__init__.py
+++ src/dcTrackClient/__init__.py
@@ -17,26 +17,97 @@ class Client:
         else:
             raise Exception('Undefined username/password or token.')
 
-    def createItem(self, data: dict):
-        """Create an item"""
-        return self.__request('POST', 'api/v2/dcimoperations/items?returnDetails=true', data)
+    # Manage Items
 
-    def modifyItem(self, id: int, data: dict):
-        """Modify an item"""
-        return self.__request('PUT', 'api/v2/dcimoperations/items/' + str(id) + '?returnDetails=true', data)
+    def createItem(self, data: dict, returnDetails: bool = True):
+        """Create a new item."""
+        return self.__request('POST', 'api/v2/dcimoperations/items?returnDetails=' + str(returnDetails).lower(), data)
+
+    def modifyItem(self, id: int, data: dict, returnDetails: bool = True):
+        """Update an existing item."""
+        return self.__request('PUT', 'api/v2/dcimoperations/items/' + str(id) + '?returnDetails=' + str(returnDetails).lower(), data)
 
     def deleteItem(self, id: int):
-        """Delete an item"""
+        """Delete an item using the item ID."""
         return self.__request('DELETE', 'api/v2/dcimoperations/items/' + str(id))
 
     def getItem(self, id: int):
-        """Get item details by ID"""
+        """Get item details using the item ID."""
         return self.__request('GET', 'api/v2/dcimoperations/items/' + str(id))
 
-    def quicksearch(self, page: int, data: dict):
-        """Search for items by specifying search criteria on available item attributes, including a multi-field search."""
-        return self.__request('POST', 'api/v2/quicksearch/items?pageSize=0&pageNumber=' + str(page), data)
+    def quicksearch(self, data: dict, pageNumber: int, pageSize: int):
+        """Search for items using criteria JSON object. Search criteria can be any of the fields applicable to items, including custom fields. Specify the fields to be included in the response. This API supports pagination."""
+        return self.__request('POST', 'api/v2/quicksearch/items?pageNumber=' + str(pageNumber) + '&pageSize=' + str(pageSize), data)
 
     def getCabinetItems(self, cabId: int):
+        """Returns a list of Items contained in a Cabinet using the ItemID of the Cabinet. The returned list includes all of the Cabinet's Items including Passive Items."""
+        return self.__request('POST', 'api/v2/items/cabinetItems/' + str(cabId))
+
+    def manageItemsBulk(self, cabId: int):
         """Retrieve a list of Items contained in a Cabinet including Passive Items."""
         return self.__request('POST', 'api/v2/items/cabinetItems/' + str(cabId))
+
+    # Makes, Models, and Connectors
+
+    def getAllMakes(self):
+        """Retrieve a list of all Makes."""
+        return self.__request('GET', '/api/v2/makes')
+
+    def addMake(self, data: dict):
+        """Add a new Make."""
+        return self.__request('POST', '/api/v2/makes', data)
+
+    def modifyMake(self, id: int, data: dict):
+        """Modify a Make."""
+        return self.__request('POST', '/api/v2/makes/' + str(id), data)
+
+    def deleteMake(self, id: int):
+        """Delete a Make."""
+        return self.__request('POST', '/api/v2/makes/' + str(id))
+
+    def getMakesByName(self, name: str, usingSpecialChars: bool = False):
+        """Search for one or more makes using the make name. You also can search using special characters."""
+        if usingSpecialChars:
+            return self.__request('POST', '/api/v2/dcimoperations/search/list/makes', {'searchString', name})
+        else:
+            return self.__request('GET', '/api/v2/dcimoperations/search/makes/' + name)
+
+    def getModel(self, id: int, usedCounts: bool = False):
+        """Get Model fields for the specified Model ID."""
+        return self.__request('GET', '/api/v2/models/' + str(id) + '?usedCounts=' + str(usedCounts).lower())
+
+    def addModel(self, data: dict, returnDetails: bool = True, proceedOnWarning: bool = False):
+        """Add a new Model."""
+        return self.__request('POST', '/api/v2/models?returnDetails=' + str(returnDetails).lower() + '&proceedOnWarning=' + str(proceedOnWarning).lower(), data)
+
+    def modifyModel(self, id: int, data: dict, returnDetails: bool = True, proceedOnWarning: bool = False):
+        """Modify an existing Model."""
+        return self.__request('PUT', '/api/v2/models/' + str(id) + '?returnDetails=' + str(returnDetails).lower() + '&proceedOnWarning=' + str(proceedOnWarning).lower(), data)
+
+    def deleteModel(self, id: int):
+        """Delete a Model using the Model ID."""
+        return self.__request('DELETE', '/api/v2/models/' + str(id))
+
+    def searchModels(self, data: dict, pageNumber: int, pageSize: int):
+        """Search for models by user supplied search criteria."""
+        return self.__request('POST', '/api/v2/quicksearch/models?pageNumber=' + str(pageNumber) + '&pageSize=' + str(pageSize), data)
+
+    def getConnector(self, id: int, usedCount: bool = False):
+        """Get a Connector record by ID."""
+        return self.__request('GET', '/api/v2/settings/connectors/' + str(id) + '?usedCount=' + str(usedCount).lower())
+
+    def addConnector(self, data: dict):
+        """Add a new Connector."""
+        return self.__request('POST', '/api/v2/settings/connectors', data)
+
+    def updateConnector(self, id: int, data: dict):
+        """Update an existing Connector."""
+        return self.__request('PUT', '/api/v2/settings/connectors/' + str(id), data)
+
+    def deleteConnector(self, ids: list[int]):
+        """Delete one or more Connector records."""
+        return self.__request('POST', '/api/v2/settings/connectors/delete', ids)
+
+    def searchConnectors(self, data: dict, pageNumber: int, pageSize: int, usedCount: bool):
+        """Retrieve a List of Connectors."""
+        return self.__request('POST', '/api/v2/settings/connectors/quicksearch?pageNumber=' + str(pageNumber) + '&pageSize=' + str(pageSize) + '&usedCount=' + str(usedCount).lower(), data)
