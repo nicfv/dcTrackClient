89538eca5a47a7e333adb7c187ec66186f750922
ventura <ventura@lbl.gov>
2023-04-19 15:12:16 -0700

    Generate documentation

---
+++ .github/workflows/doc.py
@@ -0,0 +1,16 @@
+
+with open('src/dcTrackClient/__init__.py') as f:
+    content = f.read()
+
+print('## Documentation')
+
+for line in content.split('\n'):
+    line = line.strip()
+    if line.startswith('class'):
+        print('### ' + line)
+    elif line.startswith('def') and not line.startswith('def __') or line.startswith('def __init__'):
+        print('```py')
+        print(line)
+        print('```')
+    elif line.startswith('"""'):
+        print('> ' + line.strip('"""'))
