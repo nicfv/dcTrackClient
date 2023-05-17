0098b28245d0a268520e4db0887484f0145a870f
ventura <ventura@lbl.gov>
2023-05-17 15:22:45 -0700

    Add changelog, pypi-ver to check package version on pypi, only publish if versions don't match, use version from changelog

---
+++ .github/workflows/pypi-ver.py
@@ -0,0 +1,5 @@
+import requests
+import sys
+
+print(requests.get('https://pypi.org/pypi/' +
+      sys.argv[1] + '/json').json()['info']['version'])
---
+++ CHANGELOG.md
@@ -0,0 +1,6 @@
+# Changelog
+
+## 0.4.0
+
+- First iteration of NPM package.
+- Use version number from this file.
